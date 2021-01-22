cec49a098f681ab366728b67e593fbb5a478cb13
Gurpreet Singh <cuzwhynotvlogs@gmail.com>
2021-01-22 17:33:01 -0500

    added header

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-// Assignment 1 
-// Setup web service!
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Gurpreet Singh Student ID: 074913146 Date: Jan 22/2020
+*  Heroku Link: https://agile-earth-14436.herokuapp.com
+*
+********************************************************************************/ 
 
 
 const express = require("express")
