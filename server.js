ee3afd979958286b63c5ead73429003a4414b5e9
Gurpreet Singh <cuzwhynotvlogs@gmail.com>
2021-01-22 14:46:08 -0500

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // Assignment 1 
 // Setup web service!
 
+
 const express = require("express")
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
+
+const RestaurantDB = require("./modules/restaurantDB.js");
+const db = new RestaurantDB("mongodb+srv://dbUser:money123@web422.duwz8.mongodb.net/sample_restaurants?retryWrites=true&w=majority");
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -20,3 +24,11 @@ app.get("/api/items", (req, res) => {
 app.listen(HTTP_PORT, () => {console.log(
     "Ready to handle requests on port " + HTTP_PORT
 )})
+
+db.initialize().then(()=>{
+    app.listen(HTTP_PORT, ()=>{
+        console.log(`server listening on: ${HTTP_PORT}`);
+    });
+}).catch((err)=>{
+    console.log(err);
+});
