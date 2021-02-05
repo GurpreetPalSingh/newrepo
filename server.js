d20ca4fb68f3daa406ee3f0260c968f04be52140
Gurpreet Singh <cuzwhynotvlogs@gmail.com>
2021-02-05 17:40:28 -0500

    addedd mongoose and path

--- server.js
+++ server.js
@@ -12,7 +12,8 @@
 
 const express = require("express")
 const cors = require("cors");
-const path = require("path")
+const path = require("path");
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
